eb00acc33a8ee90ce3e46819c28f847c1b51cc0a
fy <zhaoyf12138@gmail.com>
2023-04-21 11:31:10 +0800

    add deployments script

--- deploy/02-deploy-hashnft.js
+++ deploy/02-deploy-hashnft.js
@@ -4,33 +4,77 @@ const SHA256 = require('crypto-js/sha256')
 
 async function func() {
   const { deployments } = hre
-  const [deployer] = await ethers.getSigners()
-  console.log('deployer', deployer.address)
-
-  let leaves = ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"]
-  const hashFunc = x => SHA256(x)
-  const tree = new MerkleTree(leaves, hashFunc)
-  const rootHash = tree.getRoot()
+  const [deployer, issuer, tracker] = await ethers.getSigners();
+  console.log('deployer', deployer.address);
+  const block = await ethers.provider.getBlock();
+  const startAt = (Math.floor(block.timestamp / 3600 / 24) + 1) * 3600 * 24;
 
   await deployments.deploy('NFTSVG', {
-      from: deployer.address,
-      contract: 'NFTSVG',
-      log: true
-  })
-  const nftsvg = await ethers.getContract('NFTSVG')
-  const startAt = "04/01/2023"
-  await deployments.deploy('HashNFTv2', {
+    from: deployer.address,
+    contract: 'NFTSVG',
+    log: true
+  });
+  const nftsvg = await ethers.getContract('NFTSVG');
+  //wbtc
+  const decimals = 8;
+  const wbtcSupply = ethers.utils.parseUnits('100000000', decimals);
+  await deployments.deploy('WBTC', {
+    from: deployer.address,
+    contract: 'MyERC20',
+    args: ["Wrapped Bitcoin", "wbtc", wbtcSupply, decimals],
+    log: true
+  });
+  const wbtc = await ethers.getContract('WBTC');
+  await wbtc.connect(deployer).transfer(issuer.address, wbtcSupply);
+  //BitcoinEarningsOracle
+  await deployments.deploy('BitcoinEarningsOracle', {
+    from: deployer.address,
+    contract: 'BitcoinEarningsOracle',
+    log: true
+  });
+  const oracle = await ethers.getContract('BitcoinEarningsOracle');
+  await oracle.addTracker(tracker.address);
+  const f2poolHashrate = 31340
+  const f2poolDailyEarnings = 450
+  const antPoolHashrate = 201630
+  const antPoolDailyEarnings = 427
+  await oracle.connect(tracker).trackDailyEarnings([f2poolDailyEarnings, antPoolDailyEarnings], [f2poolHashrate, antPoolHashrate]);
+  //riskControlv2
+  const duration = 3600 * 24 * 30;
+  const supply = 4000;
+  const price = ethers.utils.parseEther("0.001");
+  const ratio = 3000;
+  await deployments.deploy('RiskControl', {
+    from: deployer.address,
+    contract: 'RiskControlv2',
+    args: [wbtc.address, issuer.address, price, supply, startAt, duration, ratio, oracle.address],
+    log: true
+  });
+  const riskControl = await ethers.getContract('RiskControl');
+  //hashnftv2
+  await deployments.deploy('HashNFT', {
     from: deployer.address,
     contract: 'HashNFTv2',
-    log: true,
-    libraries: { NFTSVG: nftsvg.address },
-    args: [startAt],
-  })
-
-  const hashnftv2 = await ethers.getContract('HashNFTv2') 
-  await hashnftv2.setWhiteListRootHash(rootHash)
-  await hashnftv2.setFreeMintSupply(200)
-  await hashnftv2.setWhitelistLimit(1)
+    args: ["0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"],
+    libraries: {
+      NFTSVG: nftsvg.address,
+    },
+    log: true
+  });
+  const hashnft = await ethers.getContract('HashNFT');
+  const freeMintSupply = 200;
+  const whitelistLimit = 2;
+  await hashnft.setFreeMintSupply(freeMintSupply);
+  await hashnft.setWhitelistLimit(whitelistLimit);
+  let accounts = [
+    "0xeE2dac56D96F44Adf0515a8d3c88f4B64FC7321e", "0x2568D8FA520a0887D114CA9c99b8d205Cf61dBf6", "0x328b4fd30C4FaaDa2Ae10C750c75C5f04D2C8293",
+    "0xBE830B967C6a13675cb24b35401564190b882D0E", "0xB1fcF46116022E401704Ad901bB52b486272f869", "0x1EE9ea5F314DedE44455650Ee56dAa42f1091A32",
+    "0x8F05C04370aE2C7765A60390d21Bb10DF7603285", "0x74060EA11b7d820Dd54e8356bA8969b33F33B121", "0x29BEDc56E1CbF3ebbF8763C31f1814fF36a58ed4",
+    "0x89b36C6058ec97496419BeAEC862d5B243b34c80", "0x3060A94E27b6f7A1Abe0dE2CbC90FFEc0b8D51a3", "0x425ce127A38Fb8D3De973d8aF8a5DCb4044adde1",
+  ];
+  let leaves = accounts.map(addr => ethers.utils.keccak256(addr));
+  let merkleTree = new MerkleTree(leaves, ethers.utils.keccak256, { sortPairs: true });
+  await hashnft.setWhiteListRootHash(merkleTree.getRoot());
 }
 
 module.exports = func
